d0c18a7308cf1b2f0f245fc3e1c750f3c067a747
rafroadwup-gif <rafroad.wup@fr.dtdc.com>
2026-05-09 23:23:55 +0530

    chore: add uuid to app_export

--- lib/core/app_export.dart
+++ lib/core/app_export.dart
@@ -2,6 +2,7 @@ export 'package:flutter/material.dart';
 export 'package:google_fonts/google_fonts.dart';
 export 'package:sizer/sizer.dart';
 export 'package:cached_network_image/cached_network_image.dart';
+export 'package:uuid/uuid.dart';
 export '../theme/app_theme.dart';
 export '../widgets/custom_icon_widget.dart';
 export '../widgets/custom_image_widget.dart';
